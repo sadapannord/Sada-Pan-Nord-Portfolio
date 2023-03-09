22b7df84088b631dbbf9998ad0f85409eb8998a7
thenordplace <sadapan@gmail.com>
2023-03-08 18:06:19 -0700

    fixed environment variables

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
     {
-      host: "localhost",
+      host: process.env.MYSQLHOST,
       dialect: "mysql",
-      port: 3306,
+      port: process.env.MYSQLPORT,
     }
   );
 }
